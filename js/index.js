b50c893d597ee7cd2ca588590ceeae8da09dca3a
Nimalan Suntharalinagm <nimalan@posteo.de>
2022-10-07 16:29:32 +0200

    AnswerButton

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 const bookmarkButton = document.querySelector('[data-js="card__bookmark"]');
+const cardAnswer = document.querySelector('[data-js="card__answer"]');
+const showAnswerButton = document.querySelector('[data-js="card__button-answer"]');
 
 bookmarkButton.addEventListener("click", function (){
     bookmarkButton.classList.toggle("bookmark--active");
 });
 
+showAnswerButton.addEventListener("click", () => {
+    if (showAnswerButton.textContent === "Show answer") {
+      cardAnswer.classList.add("card__answer--active");
+      showAnswerButton.textContent = "Hide answer";
+    } else {
+      cardAnswer.classList.remove("card__answer--active");
+      showAnswerButton.textContent = "Show answer";
+    }
+  });
